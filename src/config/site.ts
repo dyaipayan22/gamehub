52b67191e9ddfe4e117b35e9b0d96fc166b28087
Dyaipayan Ghosh <dev.dyaipayan@gmail.com>
2024-09-02 01:10:44 +0530

    chore: updated readme

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,21 +2,10 @@ export type SiteConfig = {
   name: string;
   description: string;
   url: string;
-  ogImage: string;
-  links: {
-    twitter: string;
-    github: string;
-  };
 };
 
 export const siteConfig: SiteConfig = {
-  name: 'RS Shonjoy',
-  description:
-    'An open source application built using the new router, server components and everything new in Next.js 13.',
+  name: 'Gamehub',
+  description: 'Live streaming platform',
   url: `${process.env.NEXT_PUBLIC_APP_URL}`,
-  ogImage: 'https://redolence.com/og.jpg',
-  links: {
-    twitter: 'https://twitter.com/rsshonjoydas',
-    github: 'https://github.com/rsshonjoydas',
-  },
 };
